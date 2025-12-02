bcb41dc9608745417541967afc9a722f7bc9499a
mihirharsoda1 <mihirharsoda1@gmail.com>
2025-12-02 19:50:29 +0530

    Mihir Patel

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -43,6 +43,115 @@ adminLogin();
                             </div>
                         </a>
                     </div>
+                    <div class="col-md-3">
+                        <a href="user_queries.php" class="text-decoration-none">
+                            <div class="card text-center text-info p-3">
+                                <h6>User Queries</h6>
+                                <h1 class="mt-2 mb-0">5</h1>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="rate_review.php" class="text-decoration-none">
+                            <div class="card text-center text-info p-3">
+                                <h6>Rating & Review</h6>
+                                <h1 class="mt-2 mb-0">5</h1>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <h5>Booking Analytics</h5>
+                    <select class="form-select shadow-none bg-light w-auto">
+                        <option selected>Open this select menu</option>
+                        <option value="1">Past 30 Days</option>
+                        <option value="2">Past 90 Days</option>
+                        <option value="3">Past 1 Year</option>
+                        <option value="4">All Time</option>
+                    </select>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-3">
+                        <div class="card text-center text-primary p-3">
+                            <h6>Total Bookings</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                            <h4 class="mt-2 mb-0">₹0</h4>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-success p-3">
+                            <h6>Active Bookings</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                            <h4 class="mt-2 mb-0">₹0</h4>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-primary p-3">
+                            <h6>Cancelled Bookings</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                            <h4 class="mt-2 mb-0">₹0</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <h5>Users, Queries, Reviews Analytics</h5>
+                    <select class="form-select shadow-none bg-light w-auto">
+                        <option selected>Open this select menu</option>
+                        <option value="1">Past 30 Days</option>
+                        <option value="2">Past 90 Days</option>
+                        <option value="3">Past 1 Year</option>
+                        <option value="4">All Time</option>
+                    </select>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-3">
+                        <div class="card text-center text-success p-3">
+                            <h6>New Registration</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-info p-3">
+                            <h6>Queries</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-primary p-3">
+                            <h6>Reviews</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <h5>Users</h5>
+                <div class="row mb-3">
+                    <div class="col-md-3">
+                        <div class="card text-center text-info p-3">
+                            <h6>Total</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-success p-3">
+                            <h6>Active</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-warning p-3">
+                            <h6>Inactive</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center text-danger p-3">
+                            <h6>Unverified</h6>
+                            <h1 class="mt-2 mb-0">0</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
